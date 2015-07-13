eeff1ae531c80ea3c154c83f75b7ffa3cab837a7
syfee <linghu.s4@gmail.com>
2015-07-13 12:12:43 +0800

    delete unnessary files

--- credentials.js
+++
@@ -1,9 +0,0 @@
-var credentials = {
-  mongolab: {
-    username: 'syfee',
-    password: '123456',
-    blog: 'mongodb://syfee:mongodb.1@ds043012.mongolab.com:43012/blog'
-  }
-};
-
-module.exports = credentials;
